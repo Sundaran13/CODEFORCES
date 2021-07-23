5ab5d986f7cef5229cd49aa2558fb79952d03b09
Sundaran <63909437+Sundaran13@users.noreply.github.com>
2021-07-23 08:46:52 +0530

    Create Harbour.Space Scholarship Contest 2021-2022 (Div. 1 + Div. 2) Editorial.cpp

---
+++ Harbour.Space Scholarship Contest 2021-2022 (Div. 1 + Div. 2) Editorial.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc..h>
+using namespace std;
+typedef long long ll;
+int main(){
+  //freopen("input.txt","r",stdin);
+  ios_base::sync_with_stdio(false);
+  int tst;
+  cin>>tst;
+  while(tst--){
+    int n;
+    cin>>n;
+    cout<<(n+1)/10<<endl;
+  }
+}
