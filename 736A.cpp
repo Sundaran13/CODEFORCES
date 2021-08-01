34bb6a5db258960ac0e7533aa773805251ecbcc4
Sundaran <63909437+Sundaran13@users.noreply.github.com>
2021-08-01 22:23:22 +0530

    Add files via upload

---
+++ 736A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+	long long int t;
+	cin>>t;
+	while(t--)
+	{
+		long long int p;
+		cin>>p;
+		cout<<2<<" "<<p-1<<endl;
+	}
+	return 0;
+}
