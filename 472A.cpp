cd1e35085086b22a0012142d0113b04ee0731e3b
Sundaran <63909437+Sundaran13@users.noreply.github.com>
2021-09-02 17:14:11 +0530

    Add files via upload

---
+++ 472A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	if(n%2!=0)
+	{
+		cout<<n-9<<" "<<9<<endl;
+	}
+	else
+	{
+		cout<<n-8<<" "<<8<<endl;
+	}
+	return 0;
+}
