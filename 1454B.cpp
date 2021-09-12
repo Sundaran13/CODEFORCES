46b459f01f7c67ffc67ad6e95565100f9ff127d2
Sundaran <63909437+Sundaran13@users.noreply.github.com>
2021-09-12 15:32:20 +0530

    Add files via upload

---
+++ 1454B.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> cnt(n+1),idx(n+1);
+		for(int i=0;i<n;i++)
+		{
+			int x;
+			cin>>x;
+			cnt[x]++;
+			idx[x]=i+1;
+		}
+		int ans=-1;
+		for(int i=0;i<=n;i++)
+		{
+			if(cnt[i]==1)
+			{
+				ans=idx[i];
+				break;
+			}
+		}
+		cout<<ans<<endl;
+	}
+	return 0;
+}
