865deb6fa96e4cbe8ab236310d1d912788ff2db6
Sundaran <63909437+Sundaran13@users.noreply.github.com>
2021-09-14 16:08:37 +0530

    Add files via upload

---
+++ 1455A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		/*int count=0;
+		long long int i=0;
+		while(n)
+		{
+			n/=10;
+			count++;
+		}
+		cout<<count<<endl;*/
+		cout<<s.length()<<endl;
+	}
+}
