e501448d4373b6eaf6bb5329eec415ef430f79a9
Sundaran <63909437+Sundaran13@users.noreply.github.com>
2021-09-15 14:03:21 +0530

    Add files via upload

---
+++ 1461A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		for(int i=0;i<n;i++)
+			cout<<char('a'+i%3);
+		cout<<endl;
+	}
+}
